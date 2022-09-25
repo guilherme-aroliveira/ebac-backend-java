3fcd334950e26ed766b2adff955ab9788b172dab
Guilherme Oliveira <oliveira.g.ar@hotmail.com>
2022-09-25 12:18:08 -0300

    add mod4 to repository

---
+++ src/mod4/Exercicio01.java
@@ -0,0 +1,29 @@
+package mod4;
+
+import java.util.Scanner;
+
+public class Exercicio01 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o ambiente: ");
+        String ambiente = sc.nextLine();
+        sc.close();
+        
+        if(ambiente.equalsIgnoreCase("DEV"))
+        {
+            System.out.println("Executando em DEV");
+        }
+        else if(ambiente.equalsIgnoreCase("TEST"))
+        {
+            System.out.println("Executando em TEST");
+        }
+        else
+        {
+            System.out.println("Executando em abiente " + ambiente);
+        }
+        System.out.println();
+    }
+}
